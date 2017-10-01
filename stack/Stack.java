29f2a991e9d22645406179d2c70e45fd05724b0a
Connor Ruggles <cruggles@iastate.edu>
2017-10-01 00:47:44 -0500

    Stack.java

--- stack/Stack.java
+++ stack/Stack.java
@@ -2,75 +2,72 @@ import java.lang.IndexOutOfBoundsException;
 import java.util.Arrays;
 
 public class Stack<E> {
-	private int max;
-	private int top;
-	private Object[] list;
+    private int max;
+    private int top;
+    private Object[] list;
 
-	public Stack() {
-		max = 512;
-		top = 0;
-		list = new Object[max];
-	}
+    public Stack() {
+        max = 512;
+        top = 0;
+        list = new Object[max];
+    }
 
-	/**
-	 * @param size starting size of the Stack
-	 */
-	public Stack(int size) {
-		max = size;
-		top = 0;
-		list = new Object[max];
-	}
+    public Stack(int size) {
+        max = size;
+        top = 0;
+        list = new Object[max];
+    }
 
-	@SuppressWarnings("unchecked")
-	public boolean isEmpty() {
-		for(E t : (E[]) list) {
-			if(t != null) {
-				return false;
-			}
-		}
+    @SuppressWarnings("unchecked")
+    public boolean isEmpty() {
+        for(E t : (E[]) list) {
+            if(t != null) {
+                return false;
+            }
+        }
 
-		return true;
-	}
+        return true;
+    }
 
-	@SuppressWarnings("unchecked")
-	public E peek() {
-		return (E) list[top];
-	}
+    @SuppressWarnings("unchecked")
+    public E peek() {
+        return (E) list[top];
+    }
 
-	@SuppressWarnings("unchecked")
-	public int size() {
-		if(top == 0) {
-			return 0;
-		}
+    @SuppressWarnings("unchecked")
+    public int size() {
+        if(top == 0) {
+            return 0;
+        }
 
-		int count = 0;
-		for(E t : (E[]) list) {
-			if(t != null) {
-				count++;
-			}
-		}
+        int count = 0;
+        for(E t : (E[]) list) {
+            if(t != null) {
+                count++;
+            }
+        }
 
-		return count;
-	}
+        return count;
+    }
 
-	@SuppressWarnings("unchecked")
-	public E pop() {
-		if(isEmpty()) {
-			throw new IndexOutOfBoundsException("This stack is empty; nothing to pop.");
-		}
+    @SuppressWarnings("unchecked")
+    public E pop() {
+        if(isEmpty()) {
+            throw new IndexOutOfBoundsException("This stack is empty; nothing to pop.");
+        }
 
-		E data = (E) list[top];
+        E data = (E) list[top];
 
-		top = (top == 0 ? 0 : top - 1);
-		return data; 
-	}
+        top = (top == 0 ? 0 : top - 1);
+        return data; 
+    }
 
-	public void push(E data) {
-		if(top == max - 1) {
-			list = Arrays.copyOf(list, list.length * 2);
-		}
+    public void push(E data) {
+        if(top == max - 1) {
+            list = Arrays.copyOf(list, list.length * 2);
+        }
 
-		top++;
-		list[top] = data;
-	}
+        top++;
+        list[top] = data;
+    }
 }
