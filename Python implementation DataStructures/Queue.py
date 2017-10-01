815ada7e4b387e4be742c9c13914bf9dbd0b9026
Shaunak Chadha <iamshaunakchadha@gmail.com>
2017-10-01 12:50:27 +0530

    PythonDs

---
+++ Python implementation DataStructures/Queue.py
@@ -0,0 +1,24 @@
+class Queue:
+	
+   	def __init__(self):
+	    self.items = []
+	
+    def isEmpty(self):
+	    return self.items == []
+	
+    def enqueue(self, item):
+	    self.items.insert(0,item)
+	
+    def dequeue(self):
+        return self.items.pop()
+	
+    def size(self):
+        return len(self.items)
+	
+
+q=Queue()
+	
+q.enqueue(4)
+q.enqueue('dog')
+q.enqueue(True)
+print(q.size())
