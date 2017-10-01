b54245b974843147f6fbf8bd4460721b70141329
Sarthak Tripathi <sarthak.trip@gmail.com>
2017-10-01 06:26:27 +0530

    fixed errors

--- seaching algorithms/binary_search.cpp
+++ seaching algorithms/binary_search.cpp
@@ -8,7 +8,7 @@ int binarySearch(int array[], int left, int right, int search)
 {
     if (right >= 1)
     {
-        int mid = 1 + (right - 1)/2;
+        int mid = ((right-1)-left)/2;
         if (array[mid] == search)                                   //Checks if the element is present at the middle
             return mid;
         if (array[mid] > search)                                    //Since this is a sorted array, therefore if value of element is less than middle
@@ -23,10 +23,10 @@ int main()
 {
     int array [] = {1,2,3,4,5,6,7,8,9,10}, search = 5, position;
     int size = sizeof(array)/ sizeof(array[0]);
-    int result = binarySearch(array, 0, size-1, search);            //here left index for binary search is sent as 0, while right as size - 1
-    if(result == -1)                                                //therefore for an array of size 10, 9 will be sent (since arrays are 0 indexed)
+    position = binarySearch(array, 0, size-1, search);            //here left index for binary search is sent as 0, while right as size - 1
+    if(position == -1)                                                //therefore for an array of size 10, 9 will be sent (since arrays are 0 indexed)
         cout<<"Element is not present in array";
     else
-        cout<<"Element is present at "<<result<<"th position";
+        cout<<"Element is present at "<<position+1<<"th position";
     return 0;
 }
