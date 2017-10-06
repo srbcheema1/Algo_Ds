0be07754a260424189792aabead7c1da39225bc9
RishabhKr bothra <rbothra77@gmail.com>
2017-10-07 01:48:59 +0530

    added more algo of patterns

---
+++ Patterns/pattern12.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+	int n;
+	cin >> n;
+
+	int count = 1;
+
+	char spaces = ' ';
+	for( int i = 1; i <= n; i++ ){
+		for( int j = 1; j < i; j++ ){
+			cout << spaces;
+		}
+		for( int j = n; j >= i; j-- ){
+			cout << count << " ";
+			count++;
+		}
+		
+		cout << endl;
+	}
+
+    return 0;
+
+}
+
+/*
+3 
+
+ 1 2 3
+  4 5
+   6
+
+*/
