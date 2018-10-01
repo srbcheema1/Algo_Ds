2b9b42004118a33ef7d259821965f27920fb36dd
Siddhant Manglik <33429059+adobe-sid@users.noreply.github.com>
2018-10-02 02:40:01 +0530

    merge sort code in python3
    Full code of merge sort in python3 with an example. Also provided the time and space complexity.

---
+++ Sorting Algorithms/Python/mergesort.py
@@ -0,0 +1,35 @@
+def mergeSort(nlist):
+
+    if len(nlist)>1:
+        mid = len(nlist)//2
+        lefthalf = nlist[:mid]
+        righthalf = nlist[mid:]
+
+        mergeSort(lefthalf)
+        mergeSort(righthalf)
+        i=j=k=0       
+        while i < len(lefthalf) and j < len(righthalf):
+            if lefthalf[i] < righthalf[j]:
+                nlist[k]=lefthalf[i]
+                i=i+1
+            else:
+                nlist[k]=righthalf[j]
+                j=j+1
+            k=k+1
+
+        while i < len(lefthalf):
+            nlist[k]=lefthalf[i]
+            i=i+1
+            k=k+1
+
+        while j < len(righthalf):
+            nlist[k]=righthalf[j]
+            j=j+1
+            k=k+1
+
+nlist = [14,46,43,27,57,41,45,21,70]
+mergeSort(nlist)
+print(nlist)
+
+# time complexity : O(nlogn)
+# space complexity: O(logn)
