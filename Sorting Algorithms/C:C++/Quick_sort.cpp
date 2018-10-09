469b366d306fdd274702e3940f7a4ed1da7d6f07
Akshay sharma <akshaysharma2277@gmail.com>
2018-10-09 15:20:21 +0530

    Create Quick_sort.cpp (#162)

---
+++ Sorting Algorithms/C:C++/Quick_sort.cpp
@@ -0,0 +1,136 @@
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+27
+28
+29
+30
+31
+32
+33
+34
+35
+36
+37
+38
+39
+40
+41
+42
+43
+44
+45
+46
+47
+48
+49
+50
+51
+52
+53
+54
+55
+56
+57
+58
+59
+60
+61
+62
+63
+64
+65
+66
+67
+68
+#include <iostream>
+ 
+using namespace std;
+ 
+void quick_sort(int[],int,int);
+int partition(int[],int,int);
+ 
+int main()
+{
+    int a[50],n,i;
+    cout<<"How many elements?";
+    cin>>n;
+    cout<<"\nEnter array elements:";
+    
+    for(i=0;i<n;i++)
+        cin>>a[i];
+        
+    quick_sort(a,0,n-1);
+    cout<<"\nArray after sorting:";
+    
+    for(i=0;i<n;i++)
+        cout<<a[i]<<" ";
+    
+    return 0;        
+}
+ 
+void quick_sort(int a[],int l,int u)
+{
+    int j;
+    if(l<u)
+    {
+        j=partition(a,l,u);
+        quick_sort(a,l,j-1);
+        quick_sort(a,j+1,u);
+    }
+}
+ 
+int partition(int a[],int l,int u)
+{
+    int v,i,j,temp;
+    v=a[l];
+    i=l;
+    j=u+1;
+    
+    do
+    {
+        do
+            i++;
+            
+        while(a[i]<v&&i<=u);
+        
+        do
+            j--;
+        while(v<a[j]);
+        
+        if(i<j)
+        {
+            temp=a[i];
+            a[i]=a[j];
+            a[j]=temp;
+        }
+    }while(i<j);
+    
+    a[l]=a[j];
+    a[j]=v;
+    
+    return(j);
+}
