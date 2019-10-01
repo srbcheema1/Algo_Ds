34a680fdd88aa80de18493d2538426973f31aa10
Pedro Dias Ambrósio Mendes <pedrod@lnxcit021923.cit>
2019-10-01 11:50:42 -0300

    Improving sieve of Erasthotenes file

--- sieve_of_eratoshthenes.cpp
+++ sieve_of_eratoshthenes.cpp
@@ -16,3 +16,30 @@ void sieve() {
     }
 }
 
+//For instance, to return the number of primes until 'n' we could do the following:
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int erasthostenesSieve(int n) {
+    vector<bool> primes(n+1,true);
+    int numberOfPrimes = 0;
+    for (int i = 2; i*i <= n; i++) {
+        if(primes[i]) {
+            for (int j = i*i; j <= n; j+=i)
+                primes[j] = false;
+        }
+    }
+    for (int p=2; p<=n; p++) {
+        if (primes[p]) {
+            numberOfPrimes++;
+        }
+    }
+    return numberOfPrimes;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    cout << erasthostenesSieve(n) << endl;
+}
