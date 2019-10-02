468dcb5af2c453593e80678dca18fe24ff83918e
Pawan Singhla <43420134+Pawansinghla@users.noreply.github.com>
2019-10-02 19:11:40 +0530

    Update LinearSearch.java
    change the variable name

--- Searching Algorithms/Java/algos/LinearSearch.java
+++ Searching Algorithms/Java/algos/LinearSearch.java
@@ -2,8 +2,8 @@ package asset;
 
 public class LinearSearch{
 	public static int linearSearch(int [] arr, int key) {
-		int size = arr.length;
-		for(int i = 0; i < size; i++) {
+		int  n= arr.length;
+		for(int i = 0; i <n; i++) {
 			if(arr[i] == key) {
 				return i;
 			}
