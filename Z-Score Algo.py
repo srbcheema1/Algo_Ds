d1d24bcebd6ab0862f5d7cac7f67ea1a6ffb4c4a
sibi812 <49360803+sibi812@users.noreply.github.com>
2019-10-20 23:49:54 +0530

    Update Z-Score Algo.py

--- Z-Score Algo.py
+++ Z-Score Algo.py
@@ -1,17 +1,17 @@
-FRAME_SIZE=10       //Number of data points you take into consideration
-MUL_FACTOR=0.005    //Amount of fine tuning you require. Smaller the factor, more is the sensitivity
+FRAME_SIZE=10       #Number of data points you take into consideration
+MUL_FACTOR=0.005    #Amount of fine tuning you require. Smaller the factor, more is the sensitivity
 
 def compute_bounds(history_data,frame_size,factor):
     if len(history_data)<frame_size :
         return None
 
     if len(history_data)>frame_size :
-        del history_data[0:len(history_data)-frame_size]   //To remove the data points apart from the last 10 points
+        del history_data[0:len(history_data)-frame_size] #To remove the data points apart from the last 10 points
     Mn=statistics.mean(history_data)                                 
     Variance=0
     for data in history_data :
         Variance += math.pow((data-Mn),2)
-    Zn = factor * math.sqrt(Variance / frame_size)                  //Process to compute dynamic thresholds
+    Zn = factor * math.sqrt(Variance / frame_size)                  #Process to compute dynamic thresholds
     High_bound = history_data[frame_size-1]+Zn
     Low_bound = history_data[frame_size-1]-Zn
     return [High_bound,Low_bound]
@@ -29,12 +29,12 @@ while True:
 	bound = compute_bounds(history_data,FRAME_SIZE,MUL_FACTOR)
     if not bound:
         required_data_count=conf.FRAME_SIZE-len(history_data)
-        print("Not enough data to compute Z-score. Need ",required_data_count," more data points")   //If data points aren't enough
+        print("Not enough data to compute Z-score. Need ",required_data_count," more data points")   #If data points aren't enough
         history_data.append(value)
         continue
 
         if sensor_value > bound[0] :
-            print ("The value increased suddenly")                //Response, in case of anomaly     
+            print ("The value increased suddenly")                #Response, in case of anomaly     
    
         elif sensor_value < bound[1]:
             print ("The value decreased suddenly")
