6b2967059ed59647a6553e0e5cdf6d5e0dca6113
Abijith Prasanthan <52949431+AbijithPrasanthan@users.noreply.github.com>
2019-10-22 11:38:59 +0530

    new pattern
    Pattern to print
    111
    222
    333
    if the input is 3.

---
+++ Patterns/Pattern17.cpp
@@ -0,0 +1,32 @@
+#include<iostream.h>
+
+using namespace std;
+
+int main()
+{
+  int n;
+  cin>>n;
+  for(int i=1;i<=n;i++)
+  {
+    for(int j=1;j<=n;j++)
+    {
+      cout<<i;
+    }
+   cout<<"\n";
+  }
+ return 0;
+}
+
+
+
+/*
+
+n=5
+
+11111
+22222
+33333
+44444
+55555
+
+*/
