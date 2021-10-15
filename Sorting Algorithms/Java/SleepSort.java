4d1573ef7fddded9e456e181a56d15e5751ba342
name <31079868+yyds5@users.noreply.github.com>
2021-10-14 20:10:53 -0400

    Update SleepSort.java
    add comment

--- Sorting Algorithms/Java/SleepSort.java
+++ Sorting Algorithms/Java/SleepSort.java
@@ -1,6 +1,10 @@
+import java.util.ArrayList;
 import java.util.concurrent.CountDownLatch;
 
 public class SleepSort {
+    //Sleep sort puts each element of an integer array into sleep at the same time
+    // due to the value differences, the smaller value would "wake up" first hence added to
+    // the array first and so on.
     private static Thread Tread;
 
     public static void main(String[] args) {
@@ -10,17 +14,23 @@ public class SleepSort {
 
     public static void sleepSort(int[] nums){
         CountDownLatch completeSignal = new CountDownLatch(nums.length);
-        for(int num : nums){
-            new Thread(() -> {
-                completeSignal.countDown();
-                try{
-                    //in order to make the sort more visible in console,
-                    // factor of 1000 milliseconds is used.
-                    completeSignal.await();
-                    Tread.sleep(num * 1000);
-                    System.out.println(num);
-                }catch(InterruptedException e){
-                    e.printStackTrace();
+        ArrayList<Integer> sortedNums = new ArrayList<>();
+        for(int i = 0; i<nums.length; i++){
+            int finalI = i;
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    completeSignal.countDown();
+                    try {
+                        completeSignal.await();
+                        //In order to make the sort more visible in console,
+                        // factor of 1000 milliseconds is applied.
+                        Tread.sleep(nums[finalI] * 1000);
+                        sortedNums.add(nums[finalI]);
+                        System.out.println("Current sorted nums: " + sortedNums.toString());
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
                 }
             }).start();
         }
